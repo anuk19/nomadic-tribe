c7e2930ec547bc12e24ad55577be2fc198f031a2
harsha8777 <harsha877@gmail.com>
2023-03-12 18:32:41 -0400

    Initial commit: Views and URLs

--- nomadic_tribe/urls.py
+++ nomadic_tribe/urls.py
@@ -15,7 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from  django.urls  import  path
+from .views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('hotel-detail/<uid>/' , hotel_detail , name="hotel_detail"),
+    path('profile/<id>/',profile_page,name='profile'),
+    path('pay_success',pay_success,name='pay_success'),
+	path('pay_cancel',pay_cancel,name='pay_cancel'),
+    path('checkout_session/<hotel_name>/<hotel_price>/<user>/<checkin>/<checkout>/<rooms>/<adventure_list>/', checkout_session, name='checkout_session'),
+    path('stripe_webhook/',stripe_webhook,name="stripe_webhook"),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+urlpatterns += staticfiles_urlpatterns()
